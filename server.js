0e4fcc107518d7843dea15a0ec5382c2dbc68616
SamriddhDiwan <samriddhdiwan7@gmail.com>
2025-03-23 16:04:04 +0530

    srever updated

--- server.js
+++ server.js
@@ -3,38 +3,41 @@ const express = require("express");
 const WebSocket = require("ws");
 const app = express();
 
-const WS_PORT = process.env.WS_PORT || 8080;
-const HTTP_PORT = process.env.HTTP_PORT || 8000;
+// Use Render's environment variables for ports
+const WS_PORT = process.env.PORT || 8080; // WebSocket port
+const HTTP_PORT = process.env.PORT || 8000; // HTTP port
 
+// WebSocket server
 const wsServer = new WebSocket.Server({ port: WS_PORT }, () =>
   console.log(`WS server is listening at ws://localhost:${WS_PORT}`)
 );
 
-// array of connected websocket clients
+// Array of connected WebSocket clients
 let connectedClients = [];
 
 wsServer.on("connection", (ws, req) => {
   console.log("Connected");
-  // add new connected client
+  // Add new connected client
   connectedClients.push(ws);
-  // listen for messages from the streamer, the clients will not send anything so we don't need to filter
+  // Listen for messages from the streamer
   ws.on("message", (data) => {
     connectedClients.forEach((ws, i) => {
       if (ws.readyState === ws.OPEN) {
-        ws.send(data);
+        ws.send(data); // Send data to all connected clients
       } else {
-        connectedClients.splice(i, 1);
+        connectedClients.splice(i, 1); // Remove disconnected clients
       }
     });
   });
 });
 
-// HTTP stuff
-app.use("/image", express.static("image"));
-app.use("/js", express.static("js"));
+// HTTP server
+app.use("/image", express.static("image")); // Serve static images
+app.use("/js", express.static("js")); // Serve static JavaScript files
 app.get("/audio", (req, res) =>
-  res.sendFile(path.resolve(__dirname, "./audio_client.html"))
+  res.sendFile(path.resolve(__dirname, "./audio_client.html")) // Serve HTML file
 );
+
 app.listen(HTTP_PORT, () =>
   console.log(`HTTP server listening at http://localhost:${HTTP_PORT}`)
 );
